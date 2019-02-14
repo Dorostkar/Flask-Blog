ed28d4ae3ed4b448f3026d22f668b0c6d4d1f9a2
Sina <sina.md88@gmail.com>
2019-02-14 15:56:29 +0000

    showing all posts releated to specific author

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -147,3 +147,15 @@ def delete_post(post_id):
     db.session.commit()
     flash('Your post has been deleted!', 'success')
     return redirect(url_for('home'))
+
+
+@app.route("/user/<string:username>")
+def user_posts(username):
+    # getting page number from url ?page=2
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username = username).first_or_404()
+    # set default to 5 record per page
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page=page, per_page=5)
+    return render_template("user_posts.html",posts=posts, user=user)
