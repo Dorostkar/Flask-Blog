44852b52358dd5ab0e1426df692de85cb1f8fff7
Sina <sina.md88@gmail.com>
2019-02-14 13:08:31 +0000

    seeing single post by idea

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -104,3 +104,9 @@ def new_post():
         flash('Your Post has been created!', 'success')
         return redirect(url_for('home'))
     return render_template('create_post.html', title="New Post", form=form)
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    # get_or_404 get me this record if not exist then send me 404
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title='post.title', post=post)
