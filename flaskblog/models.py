f491c8be61c1057f1506ab62269d4b7f318c71b5
Sina <sina.md88@gmail.com>
2019-02-22 13:18:01 +0000

    get token and verfiy token added to models

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from flaskblog import db, login_manager
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flaskblog import db, login_manager, app
 from flask_login import UserMixin
 
 @login_manager.user_loader
@@ -17,6 +18,20 @@ class User(db.Model, UserMixin):
     #backref : similar to adding new column to Post Model 
     #lazy: justify sqlAlchemy to load data as necessary in one go
     posts = db.relationship('Post',backref='author', lazy=True)
+
+    # creating token with expiration time 
+    def get_reset_token(self, expires_sec = 1800):
+        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+    
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
