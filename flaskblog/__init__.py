81233379875ec43c62a1fa910425bd5a0764448a
Sina <sina.md88@gmail.com>
2019-03-05 13:55:40 +0000

    creating Configuration class to handles config file much better

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 # pip install flask-bcrypt
@@ -6,19 +5,15 @@ from flask_bcrypt import Bcrypt
 #pip install flask-login
 from flask_login import LoginManager #for creating login system.
 from flask_mail import Mail
+from flaskblog.config import Config
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5cdcb08b231e45dcfe1be941de3b572a' #secrets.token_hex(16)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' #these three slash means relative path.
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
 mail = Mail(app)
 
 
